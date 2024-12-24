9739b7fdcc47e4f9a0e8a4b42ba51450f03fe707
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-12-24 15:11:15 -0600

    Changes:   * Update to 1.21.2 & (.3)   * Fix bug where vexes don't die if server is they are unloaded before their summoner dies

--- src/main/java/vexatious/mixin/EvokerEntity$SummonVexGoalMixin.java
+++ src/main/java/vexatious/mixin/EvokerEntity$SummonVexGoalMixin.java
@@ -1,14 +1,12 @@
 package vexatious.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.EvokerEntity;
-import net.minecraft.entity.mob.VexEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import vexatious.duck.VexEntityExtension;
 
 @Mixin(targets = "net.minecraft.entity.mob.EvokerEntity$SummonVexGoal")
@@ -17,18 +15,18 @@ public class EvokerEntity$SummonVexGoalMixin {
     @Shadow
     EvokerEntity field_7267;
 
-    @Inject(
+    @ModifyArg(
             method = "castSpell",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V"
             )
-
     )
-    private void onVexSpawned(CallbackInfo ci, @Local VexEntity vex) {
-        if (vex instanceof VexEntityExtension vexExtension) {
-            vexExtension.vexatious$setOwnerUUID(this.field_7267.getUuid());
+    private Entity modifyVexSpawn(Entity vex) {
+        if (vex instanceof VexEntityExtension extendedVex) {
+            extendedVex.vexatious$setOwnerUUID(this.field_7267.getUuid());
         }
+        return vex;
     }
 
 }
--- src/main/java/vexatious/mixin/VexEntityMixin.java
+++ src/main/java/vexatious/mixin/VexEntityMixin.java
@@ -3,6 +3,7 @@ package vexatious.mixin;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.VexEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vexatious.ActiveEvokerRegistry;
+import vexatious.Vexatious;
 import vexatious.duck.VexEntityExtension;
 
 import java.util.Optional;
@@ -19,6 +21,8 @@ import java.util.UUID;
 
 @Mixin(VexEntity.class)
 public abstract class VexEntityMixin extends HostileEntity implements VexEntityExtension {
+    @Unique
+    private static final String OWNER_UUID_KEY = Vexatious.MOD_ID + ".vex_extension.owner_uuid";
     @Unique
     @Nullable
     private UUID ownerUUID;
@@ -32,8 +36,9 @@ public abstract class VexEntityMixin extends HostileEntity implements VexEntityE
             at = @At("TAIL")
     )
     private void onPostTickVex(CallbackInfo ci) {
-        if (this.getWorld() instanceof ServerWorld serverWorld && !ActiveEvokerRegistry.get(serverWorld).containsUUID(ownerUUID)) {
-            this.kill();
+        if (this.getWorld() instanceof ServerWorld serverWorld
+                && !ActiveEvokerRegistry.get(serverWorld).containsUUID(ownerUUID)) {
+            this.kill(serverWorld);
         }
     }
 
@@ -45,5 +50,25 @@ public abstract class VexEntityMixin extends HostileEntity implements VexEntityE
     @Override
     public Optional<UUID> vexatious$getOwnerUUID() {
         return Optional.ofNullable(this.ownerUUID);
+    }
+
+    @Inject(
+            method = "readCustomDataFromNbt",
+            at = @At("TAIL")
+    )
+    private void readOwnerUUIDFromNbt(NbtCompound nbt, CallbackInfo ci) {
+        if (nbt.contains(OWNER_UUID_KEY)) {
+            this.ownerUUID = nbt.getUuid(OWNER_UUID_KEY);
+        }
+    }
+
+    @Inject(
+            method = "writeCustomDataToNbt",
+            at = @At("TAIL")
+    )
+    private void writeOwnerUUIDToNbt(NbtCompound nbt, CallbackInfo ci) {
+        if (this.ownerUUID != null) {
+            nbt.putUuid(OWNER_UUID_KEY, this.ownerUUID);
+        }
     }
 }
